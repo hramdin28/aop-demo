173e0e8d7554cb3d40f05ca3628b259da6715453
Hanish Ramdin <hanishramdindev@gmail.com>
2022-09-04 21:33:37 +0200

    Update dependencies

--- src/main/java/com/example/aop/AopDemoApplication.java
+++ src/main/java/com/example/aop/AopDemoApplication.java
@@ -1,37 +1,36 @@
 package com.example.aop;
 
+import com.example.aop.config.TeamTenantContext;
+import com.example.aop.service.InformationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-import com.example.aop.config.TeamTenantContext;
-import com.example.aop.service.InformationService;
-
 @SpringBootApplication
 public class AopDemoApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		ApplicationContext context = SpringApplication.run(AopDemoApplication.class, args);
+        ApplicationContext context = SpringApplication.run(AopDemoApplication.class, args);
 
-		InformationService informationService = (InformationService) context.getBean(InformationService.class);
+        InformationService informationService = context.getBean(InformationService.class);
 
-		TeamTenantContext.setTenantId("Liverpool");
-		displayInformation(informationService.displayInformation());
+        TeamTenantContext.setTenantId("Liverpool");
+        displayInformation(informationService.displayInformation());
 
-		TeamTenantContext.setTenantId("ManchesterUnited");
-		displayInformation(informationService.displayInformation());
+        TeamTenantContext.setTenantId("ManchesterUnited");
+        displayInformation(informationService.displayInformation());
 
-		TeamTenantContext.setTenantId("Arsenal");
-		displayInformation(informationService.displayInformation());
+        TeamTenantContext.setTenantId("Arsenal");
+        displayInformation(informationService.displayInformation());
 
-	}
+    }
 
-	private static void displayInformation(String information) {
+    private static void displayInformation(String information) {
 
-		System.out.println("-------------" + TeamTenantContext.getTenantId() + " Team Information-------------");
-		System.out.println(information);
-		System.out.println("------------------------------------------");
-	}
+        System.out.println("-------------" + TeamTenantContext.getTenantId() + " Team Information-------------");
+        System.out.println(information);
+        System.out.println("------------------------------------------");
+    }
 
 }
